75418ce3a30aa1354a91abe54097d5409894da25
Avinash Ranjan <avinashranjan268@gmail.com>
2025-10-13 19:53:54 +0530

    commit on 13 oct 2025

---
+++ Code/String/String.md
@@ -0,0 +1,22 @@
+# String
+
+## Methods
+
+1. indexOf()
+2. replace()
+3. replaceAll()
+4. padEnd()
+5. padSmart()
+6. slice()
+7. endsWith()
+8. join()
+9. splits()
+10. startsWith()
+11. toLowercase()
+12. toUppercase()
+13. trim()
+14. indexOf()
+15. match()
+16. toString()
+17. subStr()
+18. subString()
---
+++ Code/String/script.js
@@ -0,0 +1,34 @@
+// const str = "Javascript"; // First way
+// console.log(str);
+
+// const str2 = new String(10); // second way to decleare string
+
+// const x = str.toLowerCase().endsWith("script");
+// console.log(x);
+
+/*
+const arr = [
+  {
+    restaurant: "Burger King",
+    add: "Chandni Chowk",
+  },
+  {
+    restaurant: "Mac D",
+    add: "Noida 16",
+  },
+  {
+    restaurant: "Dominoz",
+    add: "Noida 15",
+  },
+];
+
+function filterRest(search) {
+  arr.filter((element) => {
+    return element.restaurant.toLowerCase();
+  });
+}
+
+const resname = prompt("Enter Restaurorent Name: ");
+filterRest(arr.toLowerCase());
+
+*/
