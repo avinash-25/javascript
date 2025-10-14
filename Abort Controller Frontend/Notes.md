d45de1a33b61686ffe8ac50478ba36c4807a86a3
Avinash Ranjan <avinashranjan268@gmail.com>
2025-10-15 00:23:40 +0530

    commit on 15 oct 2025

---
+++ Abort Controller Frontend/Notes.md
@@ -0,0 +1,7 @@
+# Abort Controller
+
+When i have to control the request response then we use Abort Controller
+
+- Signal is a property
+- abort is also a property
+- both are object
---
+++ Abort controller backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const users = [
+  {
+    userName: "Avinash",
+    age: 24,
+    city: "Noida",
+  },
+  {
+    userName: "Kanchan",
+    age: 22,
+    city: "Mathura",
+  },
+  {
+    userName: "Ranjan",
+    age: 21,
+    city: "Delhi",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("HomePage");
+});
+
+app.get("/users", (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(users);
+  }, 5000);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Your server is running at ${PORT}`);
+});
